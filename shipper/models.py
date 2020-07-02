0fe4bf86fd01a3a4409bf3d467002c67e6af3fa2
Eric Park <ideamaneric@gmail.com>
2020-07-02 23:24:12 +0800

    model: define release types in Build model

--- shipper/models.py
+++ shipper/models.py
@@ -18,13 +18,28 @@ class Variant(models.Model):
 
 # Build Model
 class Build(models.Model):
+    # Basic build information
     variant = models.ForeignKey(Variant)
     file_name = models.TextField(max_length=500)    # Bliss-v12.9-xxxx-xxxx.zip
     sourceforge_direct_link = models.URLField()     # https://sourceforge.com/xxx.zip
     size = models.IntegerField()                    # (size of file in bytes) 720924381
     type = models.TextField(max_length=20)          # official
     version = models.TextField(max_length=20)       # v12.9
-    # release = models.TextChoices()    # this needs more work
     created = models.DateTimeField(auto_now_add=True, editable=False)
+
+    # Build Release Types
+    STABLE = 'STABLE'
+    BETA = 'BETA'
+    ALPHA = 'ALPHA'
+    RELEASE_CHOICES = [
+        (STABLE, 'Stable'),
+        (BETA, 'Beta'),
+        (ALPHA, 'Alpha'),
+    ]
+    release = models.TextField(
+        max_length=10,
+        choices=RELEASE_CHOICES,
+        default=STABLE,
+    )
 
 

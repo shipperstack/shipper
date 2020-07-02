410834e2b2ff86b3277493409402748f484155db
Eric Park <ideamaneric@gmail.com>
2020-07-03 04:10:48 +0800

    shipper: add Device detail view

--- shipper/urls.py
+++ shipper/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', DashboardView.as_view(), name='dashboard')
+    path('', DashboardView.as_view(), name='dashboard'),
+    path('device/<int:pk>/', DeviceDetailView.as_view(), name='device_detail')
 ]
--- shipper/views.py
+++ shipper/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import *
@@ -12,3 +12,8 @@ class DashboardView(LoginRequiredMixin, ListView):
     # Override devices shown to maintainers
     def get_queryset(self):
         return Device.objects.filter(maintainers=self.request.user)
+
+
+class DeviceDetailView(LoginRequiredMixin, DetailView):
+    template_name = 'shipper/device_detail.html'
+    model = Device

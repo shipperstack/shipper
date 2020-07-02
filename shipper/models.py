53a060dadc36cb0e21d124f763b8798944af9a1d
Eric Park <ideamaneric@gmail.com>
2020-07-02 23:52:59 +0800

    model: add CPU and GPU information to Device model

--- shipper/models.py
+++ shipper/models.py
@@ -7,9 +7,12 @@ class Device(models.Model):
     name = models.TextField()                       # Nexus 5X
     codename = models.TextField(max_length=50)      # bullhead
     manufacturer = models.TextField(max_length=20)  # LG
+    cpu = models.TextField(max_length=50)           # MSM8940
+    gpu = models.TextField(max_length=50)           # Adreno 420
     memory = models.IntegerField(max_length=4)      # 2 (in gigabytes)
     storage = models.IntegerField(max_length=10)    # 512 (in gigabytes)
     photo = models.URLField()                       # Image of device
+    status = models.BooleanField(default=True)      # True - device is still maintained
     maintainers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="devices")
     created = models.DateTimeField(auto_now_add=True, editable=False)
 

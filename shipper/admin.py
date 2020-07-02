c5b59ee3f8c8670f5ca121b0d1ab3abea25b17dd
Eric Park <ideamaneric@gmail.com>
2020-07-03 00:06:09 +0800

    admin: register DeviceAdmin

--- shipper/admin.py
+++ shipper/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = ['id', 'codename', 'created']
+
+
+admin.site.register(Device, DeviceAdmin)
+

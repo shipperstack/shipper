c94645eb4108001d71c5a6590421fac9860dcdb9
Eric Park <ideamaneric@gmail.com>
2020-07-03 17:22:53 +0800

    config: set up media directory

--- config/settings.py
+++ config/settings.py
@@ -127,4 +127,13 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 
+# Media
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# Login
+
 LOGIN_REDIRECT_URL = '/'

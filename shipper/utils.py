8067e53defb2d7538b7968044e79d75a880ae7b8
Eric Park <ideamaneric@gmail.com>
2020-07-03 22:29:21 +0800

    shipper: add helper utils file

---
+++ shipper/utils.py
@@ -0,0 +1,9 @@
+import os
+
+from django.conf import settings
+
+
+# Delete build artifact and MD5 hash
+def delete_artifact(codename, file_path):
+    os.remove(os.path.join(settings.MEDIA_ROOT, codename, file_path))
+    os.remove(os.path.join(settings.MEDIA_ROOT, codename, file_path, ".md5"))

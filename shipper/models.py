9ff71364595085c55670c33ba978b2041e583e6c
Eric Park <ideamaneric@gmail.com>
2020-07-04 09:23:44 +0800

    shipper: model: remove SourceForge direct link
    We can recreate the link from file_name only

--- shipper/models.py
+++ shipper/models.py
@@ -55,9 +55,6 @@ class Build(models.Model):
         max_length=500,
         unique=True,
         help_text="Example: 'Bliss-v12.8-bullhead-OFFICIAL-20200608")
-    sourceforge_direct_link = models.URLField(
-        help_text="https://sourceforge.net/projects/blissroms/files/Q/<codename>/<file_name>.zip/download"
-    )
     size = models.IntegerField(
         help_text="Size of zip file in bytes<br>Example: 857483855"
     )

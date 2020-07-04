d4622e2834b8a17fc36a476becc191ef2fc3eb9d
Eric Park <ideamaneric@gmail.com>
2020-07-04 11:09:14 +0800

    shipper: utils: do not crash if files do not exist

--- shipper/utils.py
+++ shipper/utils.py
@@ -5,5 +5,8 @@ from django.conf import settings
 
 # Delete build artifact and MD5 hash
 def delete_artifact(codename, file_path):
-    os.remove(os.path.join(settings.MEDIA_ROOT, codename, file_path))
-    os.remove(os.path.join(settings.MEDIA_ROOT, codename, file_path, ".md5"))
+    try:
+        os.remove(os.path.join(settings.MEDIA_ROOT, codename, file_path))
+        os.remove(os.path.join(settings.MEDIA_ROOT, codename, file_path, ".md5"))
+    except OSError:
+        pass

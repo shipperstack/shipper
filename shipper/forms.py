9606f699f45fde869e047600f41025faed9b7cf0
Eric Park <ideamaneric@gmail.com>
2020-07-04 11:32:35 +0800

    shipper: add more fields to upload form

--- shipper/forms.py
+++ shipper/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 from .validators import validate_file_extension
+from .models import Build
 
 
 class BuildUploadForm(forms.Form):
@@ -8,3 +9,5 @@ class BuildUploadForm(forms.Form):
         widget=forms.ClearableFileInput(attrs={'multiple': True}),
         validators=[validate_file_extension]
     )
+    gapps = forms.BooleanField()
+    release = forms.ChoiceField(choices=Build.RELEASE_CHOICES)

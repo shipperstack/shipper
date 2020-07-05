96061c5829a8a04ee6eca777240d54cd5d18ff23
Eric Park <ideamaneric@gmail.com>
2020-07-06 05:12:43 +0800

    accounts: add password change view

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', auth_view.LoginView.as_view(), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', register, name='register'),
+    path('password_change/', auth_view.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
 ]

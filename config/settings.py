b985bfe20fc41bcdc5d7f7fdeeaf7bde2737d1fc
Eric Park <ideamaneric@gmail.com>
2020-07-24 16:01:22 +0800

    Set framework defaults

--- config/settings.py
+++ config/settings.py
@@ -144,3 +144,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Login
 
 LOGIN_REDIRECT_URL = '/maintainers/'
+
+# Django REST Framework
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated', )
+}

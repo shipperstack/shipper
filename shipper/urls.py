ec4a1a1c5d46628c65e5e2da542bf0a4b916bd64
Eric Park <ideamaneric@gmail.com>
2020-08-05 19:56:20 +0900

    shipper: urls: add missing end slash

--- shipper/urls.py
+++ shipper/urls.py
@@ -5,11 +5,11 @@ urlpatterns = [
     path('', DownloadsView.as_view(), name='downloads'),
     path('maintainers/', MaintainerDashboardView.as_view(), name='dashboard'),
     path('maintainers/device/<int:pk>/', DeviceDetailView.as_view(), name='device_detail'),
-    path('maintainers/device/<int:pk>/dfp', device_force_processing, name='device_force_processing'),
+    path('maintainers/device/<int:pk>/dfp/', device_force_processing, name='device_force_processing'),
     path('maintainers/device/<int:pkd>/build/<int:pk>/', BuildDetailView.as_view(), name='build_detail'),
     path('maintainers/device/<int:pkd>/build/<int:pk>/delete/', BuildDeleteView.as_view(), name='build_delete'),
     path('maintainers/device/<int:pk>/upload/', build_upload, name='build_upload'),
-    path('maintainers/api/login', maintainer_api_login, name='maintainer_api_login'),
+    path('maintainers/api/login/', maintainer_api_login, name='maintainer_api_login'),
     path('maintainers/api/device/id/', maintainer_api_device_id, name='maintainer_api_device_id'),
     path('maintainers/api/device/<int:pk>/upload/', maintainer_api_build_upload, name='maintainer_api_build_upload'),
 ]

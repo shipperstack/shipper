b6f3a084a95a957eeb550ef8ad8e93a6e8b83596
Eric Park <ideamaneric@gmail.com>
2020-08-09 15:15:10 +0900

    accounts: fix password change screen

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', auth_view.LoginView.as_view(), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', register, name='register'),
-    path('password_change/', auth_view.PasswordChangeView.as_view(template_name='registration/password_change.html'), name='password_change'),
+    path('password_change/', auth_view.PasswordChangeView.as_view(), name='password_change'),
+    path('password_change_done/', auth_view.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]

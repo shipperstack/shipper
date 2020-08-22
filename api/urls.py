bcf7bbebb760930c40bd2b14ddec51a00e229368
Eric Park <ideamaneric@gmail.com>
2020-08-22 14:26:14 +0900

    api: fix broken URL

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from .views import *
 
 urlpatterns = [
     path('v1/updater/', v1_updater, name='v1_updater'),
-    path('v1/updater/<slug:codename>/<slug:release>', v1_updater_device, name='v1_updater_device'),
+    path('v1/updater/<slug:codename>/<slug:release>/', v1_updater_device, name='v1_updater_device'),
 ]

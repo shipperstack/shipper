b28f587343604d382fedfe4045034a9b100f9a35
Eric Park <ideamaneric@gmail.com>
2020-08-22 14:22:18 +0900

    api: support the legacy updater API

--- api/views.py
+++ api/views.py
@@ -1,8 +1,8 @@
 import json
 
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from shipper.models import *
 
 
@@ -30,4 +30,46 @@ def v1_updater(request):
         }
 
         retJson[codename] = [deviceJson]
+    return HttpResponse(json.dumps(retJson), content_type='application/json')
+
+def v1_updater_device(request, codename, release):
+
+    # Check if device exists
+    device = get_object_or_404(Device, codename=codename)
+
+    try:
+        build = device.get_latest_build_object()
+    except Build.DoesNotExist:
+        raise Http404("No builds exist for this device yet!")
+    _, version, codename, type, date = build.file_name.split('-')
+
+    # Convert date into UNIX time
+    year = int(date[:4])
+    month = int(date[4:-2])
+    day = int(date[6])
+
+    import datetime
+    date = datetime.date(year, month, day)
+
+    """
+    Yes this is a horrifying JSON structure but the legacy system requires it.
+    """
+    retJson = {
+        "response": [
+                {
+                    "datetime": int(date.strftime("%s")),
+                    "filename": "{}.zip".format(build.file_name),
+                    "id": build.sha256sum,
+                    "romtype": "official",
+                    "size": build.size,
+                    "url": "https://sourceforge.net/projects/{}/files/Q/{}/{}/download".format(
+                               settings.SOURCEFORGE_PROJECT,
+                               device.codename,
+                               "{}.zip".format(build.file_name)
+                    ),
+                    "version": build.version,
+                }
+        ]
+    }
+
     return HttpResponse(json.dumps(retJson), content_type='application/json')

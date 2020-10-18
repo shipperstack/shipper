d968fc19d0c77f3e71150134c9917dca44bc1735
Eric Park <me@ericswpark.com>
2020-10-18 11:38:10 +0900

    shipper: tasks: upload to R directory now

--- shipper/tasks.py
+++ shipper/tasks.py
@@ -43,7 +43,7 @@ def process_build(codename):
                             os.path.join(
                                 '/home/frs/project/',
                                 settings.SOURCEFORGE_PROJECT,
-                                'Q'
+                                'R'
                             )
                         )
 

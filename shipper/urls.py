4758c942da05285baabfd7c9bb0d4a8acfc40353
Eric Park <me@ericswpark.com>
2020-10-21 18:59:37 +0900

    shipper: urls: delete force processing link

--- shipper/urls.py
+++ shipper/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', DownloadsView.as_view(), name='downloads'),
     path('maintainers/', MaintainerDashboardView.as_view(), name='dashboard'),
     path('maintainers/device/<int:pk>/', DeviceDetailView.as_view(), name='device_detail'),
-    path('maintainers/device/<int:pk>/dfp/', device_force_processing, name='device_force_processing'),
     path('maintainers/device/<int:pkd>/build/<int:pk>/', BuildDetailView.as_view(), name='build_detail'),
     path('maintainers/device/<int:pkd>/build/<int:pk>/delete/', BuildDeleteView.as_view(), name='build_delete'),
     path('maintainers/device/<int:pk>/upload/', build_upload, name='build_upload'),

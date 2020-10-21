949c02d46fc028f60042cf0027e4f2121afe70f0
Eric Park <me@ericswpark.com>
2020-10-21 20:45:07 +0900

    api: remove all v1 API
    Deprecated because we no longer use SF and we are not supporting Q-based builds

--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,4 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('v1/updater/', v1_updater, name='v1_updater'),
-    path('v1/updater/<slug:codename>/<slug:release>/', v1_updater_device, name='v1_updater_device'),
 ]

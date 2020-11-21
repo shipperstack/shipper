2856e9f11360b1d726f07af9db4f35cbc9829683
Eric Park <me@ericswpark.com>
2020-11-21 12:54:31 +0900

    config: settings: fix DB default values

--- config/settings.py
+++ config/settings.py
@@ -83,10 +83,10 @@ DATABASES = {
     'default': {
         'ENGINE': env("SQL_ENGINE", default='django.db.backends.sqlite3'),
         'NAME': env("SQL_DATABASE", default=os.path.join(BASE_DIR, 'db.sqlite3')),
-        "USER": env("SQL_USER", "user"),
-        "PASSWORD": env("SQL_PASSWORD", "password"),
-        "HOST": env("SQL_HOST", "localhost"),
-        "PORT": env("SQL_PORT", "5432"),
+        'USER': env("SQL_USER", default="user"),
+        'PASSWORD': env("SQL_PASSWORD", default="password"),
+        'HOST': env("SQL_HOST", default="localhost"),
+        'PORT': env("SQL_PORT", default="5432"),
     }
 }
 

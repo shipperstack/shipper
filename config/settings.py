12e8544298325e0eb9ceca83e7364202289acc50
Eric Park <me@ericswpark.com>
2020-11-25 20:12:17 +0900

    config: settings: remove STATICFILES_DIR configuration
    Don't need it for now

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Media
 MEDIA_URL = '/media/'

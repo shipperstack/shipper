467cd0739a0e6a37af73b524f4581151223b9ecc
Eric Park <me@ericswpark.com>
2021-05-22 21:02:27 +0900

    entrypoint: use correct variable names

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$SHIPPER_SQL_ENGINE" = "django.db.backends.postgresql" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SQL_HOST $SQL_PORT; do
+    while ! nc -z $SHIPPER_SQL_HOST $SHIPPER_SQL_PORT; do
       sleep 0.1
     done
 

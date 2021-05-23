669b289271c154a5b4c18eee9f3ecd89bcb13c48
Eric Park <me@ericswpark.com>
2021-05-23 14:14:22 +0900

    Add tasks.py

---
+++ shipper/tasks.py
@@ -0,0 +1,28 @@
+import os
+import pysftp
+
+from celery import shared_task
+from config import settings
+
+
+@shared_task
+def backup_build(build):
+    with pysftp.Connection(
+            host="frs.sourceforge.net",
+            username=settings.SHIPPER_SF_USERNAME,
+            private_key=settings.SHIPPER_SF_PRIVATE_KEY,
+    ) as sftp:
+        sftp.cwd(
+            os.path.join(
+                '/home/frs/project/',
+                settings.SHIPPER_SF_PATH,
+                build.device.codename
+            )
+        )
+
+        sftp.put(os.path.join(settings.MEDIA_ROOT, build.zip_file))
+        sftp.put(os.path.join(settings.MEDIA_ROOT, build.md5_file))
+
+    build.backed_up = True
+    build.save()
+

97e29e36a890f2e4c82e684ec7019b47b4f3db95
Eric Park <me@ericswpark.com>
2021-05-24 15:04:54 +0900

    shipper: admin: show full name

--- shipper/admin.py
+++ shipper/admin.py
@@ -6,12 +6,17 @@ from .models import *
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ['username', 'is_active', 'email', 'last_login', 'get_devices', 'is_staff', 'is_superuser']
+    list_display = ['username', 'is_active', 'get_full_name', 'email', 'last_login', 'get_devices', 'is_staff',
+                    'is_superuser']
     ordering = ['-last_login']
 
     def get_devices(self, obj):
         return [device.codename for device in Device.objects.filter(maintainers=obj)]
     get_devices.short_description = 'Devices'
+
+    def get_full_name(self, obj):
+        return "{} {}".format(obj.first_name, obj.last_name)
+    get_full_name.short_description = "Full Name"
 
 
 class DeviceAdmin(admin.ModelAdmin):

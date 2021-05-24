ebcf451aced2806acc8ec30c3436144db4da9987
Eric Park <me@ericswpark.com>
2021-05-24 16:54:59 +0900

    Create command to finalize incomplete builds
    In case the process is interrupted (or if Celery goes down)

---
+++ shipper/management/commands/finalize_build_hashes.py
@@ -0,0 +1,18 @@
+from django.core.management import BaseCommand
+
+from shipper.models import Build
+from shipper.tasks import generate_sha256
+
+
+class Command(BaseCommand):
+    help = "Calculates hashes for incomplete builds with interrupted processing."
+
+    def handle(self, *args, **options):
+        builds = Build.objects.filter(sha256sum__exact='')
+
+        for build in builds:
+            self.stdout.write("Queueing re-calculation of SHA256 hash for build {}...".format(build.file_name))
+            generate_sha256.delay(build.id)
+
+        self.stdout.write("All incompleted builds have been queued for processing. Please check the admin panel for "
+                          "status updates.")

5f6e99a4ad83bf50da5311afa766bda85dd07369
Eric Park <me@ericswpark.com>
2021-05-24 20:36:55 +0900

    shipper: tasks: set up lock for upload task

--- shipper/tasks.py
+++ shipper/tasks.py
@@ -1,17 +1,32 @@
 import hashlib
 import os
+import time
+from contextlib import contextmanager
 
 import paramiko
 import pysftp
 from celery import shared_task
+from django.core.cache import cache
 from paramiko.py3compat import decodebytes
 
 from config import settings
 from shipper.models import Build
 
 
-@shared_task
-def backup_build(build_id):
+@contextmanager
+def memcache_lock(lock_id, oid):
+    timeout_at = time.monotonic() + settings.CELERY_TASK_TIME_LIMIT - 3
+    status = cache.add(lock_id, oid, settings.CELERY_TASK_TIME_LIMIT)
+    try:
+        yield status
+    finally:
+        if time.monotonic() < timeout_at and status:
+            cache.delete(lock_id)
+
+
+# noinspection SpellCheckingInspection
+@shared_task(bind=True)
+def backup_build(self, build_id):
     build = Build.objects.get(id=build_id)
 
     # Check if backup is enabled
@@ -23,37 +38,44 @@ def backup_build(build_id):
     if build.backed_up:
         return
 
-    keydata = b"""AAAAB3NzaC1yc2EAAAABIwAAAQEA2uifHZbNexw6cXbyg1JnzDitL5VhYs0E65Hk/tLAPmcmm5GuiGeUoI
-/B0eUSNFsbqzwgwrttjnzKMKiGLN5CWVmlN1IXGGAfLYsQwK6wAu7kYFzkqP4jcwc5Jr9UPRpJdYIK733tSEmzab4qc5Oq8izKQKIaxXNe7FgmL15HjSpatF
-t9w/ot/CHS78FUAr3j3RwekHCm/jhPeqhlMAgC+jUgNJbFt3DlhDaRMa0NYamVzmX8D47rtmBbEDU3ld6AezWBPUR5Lh7ODOwlfVI58NAf/aYNlmvl2TZiau
-BCTa7OPYSyXJnIPbQXg6YQlDknNCr0K769EjeIlAfY87Z4tw=="""
-    key = paramiko.RSAKey(data=decodebytes(keydata))
-    cnopts = pysftp.CnOpts()
-    cnopts.hostkeys.add('frs.sourceforge.net', 'ssh-rsa', key)
+    # Setup lock
+    lock_id = '{}-lock-{}'.format(self.name, build.id)
+    with memcache_lock(lock_id, self.app.oid) as acquired:
+        if acquired:
+            keydata = b"AAAAB3NzaC1yc2EAAAABIwAAAQEA2uifHZbNexw6cXbyg1JnzDitL5VhYs0E65Hk/tLAPmcmm5GuiGeUoI" \
+                      b"/B0eUSNFsbqzwgwrttjnzKMKiGLN5CWVmlN1IXGGAfLYsQwK6wAu7kYFzkqP4jcwc5Jr9UPRpJdYIK733t" \
+                      b"SEmzab4qc5Oq8izKQKIaxXNe7FgmL15HjSpatFt9w/ot/CHS78FUAr3j3RwekHCm/jhPeqhlMAgC+jUgNJ" \
+                      b"bFt3DlhDaRMa0NYamVzmX8D47rtmBbEDU3ld6AezWBPUR5Lh7ODOwlfVI58NAf/aYNlmvl2TZiauBCTa7O" \
+                      b"PYSyXJnIPbQXg6YQlDknNCr0K769EjeIlAfY87Z4tw=="
+            key = paramiko.RSAKey(data=decodebytes(keydata))
+            cnopts = pysftp.CnOpts()
+            cnopts.hostkeys.add('frs.sourceforge.net', 'ssh-rsa', key)
 
-    with pysftp.Connection(
-            host="frs.sourceforge.net",
-            username=settings.SHIPPER_SF_USERNAME,
-            private_key=settings.SHIPPER_SF_PRIVATE_KEY,
-            cnopts=cnopts
-    ) as sftp:
-        sftp.cwd(
-            os.path.join(
-                '/home/frs/project/',
-                settings.SHIPPER_SF_PATH,
-            )
-        )
+            with pysftp.Connection(
+                    host="frs.sourceforge.net",
+                    username=settings.SHIPPER_SF_USERNAME,
+                    private_key=settings.SHIPPER_SF_PRIVATE_KEY,
+                    cnopts=cnopts
+            ) as sftp:
+                sftp.cwd(
+                    os.path.join(
+                        '/home/frs/project/',
+                        settings.SHIPPER_SF_PATH,
+                    )
+                )
 
-        if not sftp.exists(build.device.codename):
-            sftp.mkdir(build.device.codename)
+                if not sftp.exists(build.device.codename):
+                    sftp.mkdir(build.device.codename)
 
-        sftp.cwd(build.device.codename)
+                sftp.cwd(build.device.codename)
 
-        sftp.put(os.path.join(settings.MEDIA_ROOT, build.zip_file.name))
-        sftp.put(os.path.join(settings.MEDIA_ROOT, build.md5_file.name))
+                sftp.put(os.path.join(settings.MEDIA_ROOT, build.zip_file.name))
+                sftp.put(os.path.join(settings.MEDIA_ROOT, build.md5_file.name))
 
-    build.backed_up = True
-    build.save()
+            build.backed_up = True
+            build.save()
+        else:
+            print("Build {} is already being uploaded by another process, exiting!".format(build.file_name))
 
 
 @shared_task

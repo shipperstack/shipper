82ca6aea4e3415a78d2e8eb47e667dde9db1cc50
Eric Park <me@ericswpark.com>
2021-05-24 14:50:25 +0900

    accounts: forms: force email requirement

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,12 +3,20 @@ from django import forms
 
 
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(RegisterForm, self).__init__(*args, **kwargs)
+
+        for field in self.Meta.required:
+            self.fields[field].required = True
+            self.fields[field].help_text = "Required."
+
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     password_verify = forms.CharField(label='Verify Password', widget=forms.PasswordInput)
 
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name', 'email']
+        required = ['email']    # Warning: do not put username as required (already is)
 
     def clean_password_verify(self):
         cd = self.cleaned_data

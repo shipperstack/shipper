50cc91efe11c2c10ff907c328badab69754167df
Eric Park <me@ericswpark.com>
2021-05-24 14:15:49 +0900

    shipper: admin: show backed_up field as column

--- shipper/admin.py
+++ shipper/admin.py
@@ -20,7 +20,8 @@ class DeviceAdmin(admin.ModelAdmin):
 
 
 class BuildAdmin(admin.ModelAdmin):
-    list_display = ['id', 'file_name', 'get_device_name', 'size', 'version', 'variant', 'get_build_device_maintainers', 'created']
+    list_display = ['id', 'file_name', 'get_device_name', 'size', 'version', 'variant', 'get_build_device_maintainers',
+                    'backed_up', 'created']
     ordering = ['-created']
 
     def get_device_name(self, obj):

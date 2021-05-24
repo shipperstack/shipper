f3aeca4829710976d8b5fd638c4b7e354014631d
Eric Park <me@ericswpark.com>
2021-05-24 09:59:22 +0900

    entrypoint: add debug statement

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,7 @@ then
 
     while ! nc -z $SHIPPER_SQL_HOST $SHIPPER_SQL_PORT; do
       sleep 0.1
+      echo "Still waiting... If you see this multiple times then we can't find postgresql."
     done
 
     echo "PostgreSQL started"

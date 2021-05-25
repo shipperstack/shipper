12578ca95500341e96b875ae40aca70b7720ca55
Eric Park <me@ericswpark.com>
2021-05-25 14:40:06 +0900

    shipper: admin: sort by active users first

--- shipper/admin.py
+++ shipper/admin.py
@@ -7,7 +7,7 @@ from .models import *
 
 class CustomUserAdmin(UserAdmin):
     list_display = ['username', 'is_active', 'email', 'last_login', 'get_devices', 'is_staff', 'is_superuser']
-    ordering = ['-last_login']
+    ordering = ['-is_active', '-last_login']
 
     def get_devices(self, obj):
         return [device.codename for device in Device.objects.filter(maintainers=obj)]

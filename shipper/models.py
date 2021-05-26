e39fb576e50678123d32d85c13443788255078fd
Eric Park <me@ericswpark.com>
2021-05-26 15:33:06 +0900

    shipper: models: add missing commas

--- shipper/models.py
+++ shipper/models.py
@@ -120,12 +120,12 @@ class MirrorServer(models.Model):
     upload_path = models.TextField(
         max_length=100,
         help_text='Path to upload to on the server.<br>'
-                  'Example: /home/frs/project/example/R/, /mnt/media/mirror/src/target/R/, etc.'
+                  'Example: /home/frs/project/example/R/, /mnt/media/mirror/src/target/R/, etc.',
     )
     enabled = models.BooleanField(
         default=True,
         help_text='Whether this mirror instance is enabled or not. If disabled, builds will not be mirrored until '
-                  'the mirror instance is enabled again and a background refresh task runs.'
+                  'the mirror instance is enabled again and a background refresh task runs.',
     )
 
     def __str__(self):

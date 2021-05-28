8b94c3c3547a152231d44070b8254fed889bc270
Eric Park <me@ericswpark.com>
2021-05-28 09:29:51 +0900

    shipper: tests: add image URL test

--- shipper/tests.py
+++ shipper/tests.py
@@ -19,10 +19,26 @@ class DeviceTestCase(TestCase):
             photo="https://fdn2.gsmarena.com/vv/bigpic/huawei-nexus-6p-.jpg",
             status=False
         )
+        Device.objects.create(
+            name="Galaxy S8+",
+            codename="dream2lte",
+            manufacturer="Samsung",
+            status=True
+        )
 
     def test_device_string(self):
         bullhead = Device.objects.get(codename="bullhead")
         angler = Device.objects.get(codename="angler")
+        dream2lte = Device.objects.get(codename="dream2lte")
         self.assertEqual(str(bullhead), "LG Nexus 5X (bullhead)")
         self.assertEqual(str(angler), "Huawei Nexus 6P (angler)")
+        self.assertEqual(str(dream2lte), "Samsung Galaxy S8+ (dream2lte)")
+
+    def test_image_url(self):
+        bullhead = Device.objects.get(codename="bullhead")
+        angler = Device.objects.get(codename="angler")
+        dream2lte = Device.objects.get(codename="dream2lte")
+        self.assertEqual(bullhead.get_photo_url(), "https://fdn2.gsmarena.com/vv/bigpic/lg-nexus-5x-.jpg")
+        self.assertEqual(angler.get_photo_url(), "https://fdn2.gsmarena.com/vv/bigpic/huawei-nexus-6p-.jpg")
+        self.assertEqual(dream2lte.get_photo_url(), "#")
 

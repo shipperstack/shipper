276a1bae0cde20417345775cd579bb92c0af8887
Eric Park <me@ericswpark.com>
2021-05-28 09:26:15 +0900

    shipper: tests: add Device test cases

--- shipper/tests.py
+++ shipper/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
 
-# Create your tests here.
+from shipper.models import Device
+
+
+class DeviceTestCase(TestCase):
+    def setUp(self):
+        Device.objects.create(
+            name="Nexus 5X",
+            codename="bullhead",
+            manufacturer="LG",
+            photo="https://fdn2.gsmarena.com/vv/bigpic/lg-nexus-5x-.jpg",
+            status=True
+        )
+        Device.objects.create(
+            name="Nexus 6P",
+            codename="angler",
+            manufacturer="Huawei",
+            photo="https://fdn2.gsmarena.com/vv/bigpic/huawei-nexus-6p-.jpg",
+            status=False
+        )
+
+    def test_device_string(self):
+        bullhead = Device.objects.get(codename="bullhead")
+        angler = Device.objects.get(codename="angler")
+        self.assertEqual(str(bullhead), "LG Nexus 5X (bullhead)")
+        self.assertEqual(str(angler), "Huawei Nexus 6P (angler)")
+

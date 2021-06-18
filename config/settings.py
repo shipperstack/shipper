cd91666c3f797e41595e06e169077466d7953e97
Eric Park <me@ericswpark.com>
2021-06-18 14:58:25 +0900

    config: settings: suppress PyCharm warning
    It does exist...

--- config/settings.py
+++ config/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 STATIC_URL = '/static/'
+# noinspection PyUnresolvedReferences
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media

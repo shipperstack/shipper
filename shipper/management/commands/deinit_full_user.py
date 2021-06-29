a60220f0d73feaef7ba8ba1b4f20d21c7d5851a9
Eric Park <me@ericswpark.com>
2021-06-29 15:58:57 +0900

    Add command to de-initialize full user

--- shipper/management/commands/deinit_full_user.py
+++ shipper/management/commands/deinit_full_user.py
@@ -5,17 +5,17 @@ from shipper.models import Device
 
 
 class Command(BaseCommand):
-    help = "Initializes a user with access to all enabled devices."
+    help = "De-initializes a full user by removing access to all enabled devices."
 
     def add_arguments(self, parser):
-        parser.add_argument('username', nargs=1, action='store', help='Username of user to be init')
+        parser.add_argument('username', nargs=1, action='store', help='Username of user to be de-init')
 
     def handle(self, *args, **options):
-        self.stdout.write("Initializing user {}. Do you want to continue? [y/N] ".format(options['username'][0]),
+        self.stdout.write("De-initializing user {}. Do you want to continue? [y/N] ".format(options['username'][0]),
                           ending='')
 
         if input().lower() == 'y':
-            self.stdout.write("Starting initialization...")
+            self.stdout.write("Starting removal...")
 
             # Make sure the user exists within the system
             try:
@@ -24,20 +24,20 @@ class Command(BaseCommand):
                 self.stdout.write(self.style.ERROR("The specified user does not exist!"))
                 return
 
-            # Add user to all existing devices
+            # Remove user from all existing devices
             devices = Device.objects.filter(status=True)
 
             for device in devices:
-                self.stdout.write("Adding device {}... ".format(device), ending='')
+                self.stdout.write("Removing device {}... ".format(device), ending='')
 
                 # Check if user is already a maintainer
-                if user in device.maintainers.all():
-                    self.stdout.write(self.style.WARNING("Skipped (already added)"))
+                if user not in device.maintainers.all():
+                    self.stdout.write(self.style.WARNING("Skipped (already removed)"))
                 else:
-                    device.maintainers.add(user)
-                    self.stdout.write(self.style.SUCCESS("Added"))
+                    device.maintainers.remove(user)
+                    self.stdout.write(self.style.SUCCESS("Removed"))
 
-            self.stdout.write("The specified user has been added to all enabled devices!")
+            self.stdout.write("The specified user has been removed from all enabled devices!")
         else:
             self.stdout.write("Exiting...")
             return

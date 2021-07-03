194c6d42502440ec6a76ba7d7502b9847775cf58
Eric Park <me@ericswpark.com>
2021-07-03 21:43:48 +0900

    Check for debug mode and alert user

--- config/context_processors.py
+++ config/context_processors.py
@@ -3,3 +3,7 @@ from django.conf import settings
 
 def version_processor(request):
     return {'SHIPPER_VERSION': settings.SHIPPER_VERSION}
+
+
+def debug_mode_processor(request):
+    return {'DEBUG': settings.DEBUG}

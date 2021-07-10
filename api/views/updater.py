1dac6f1ecab20dddf158b93f347380f8095eae6c
Eric Park <me@ericswpark.com>
2021-07-10 12:41:41 +0900

    api: views: updater: return 400 if variant is invalid

--- api/views/updater.py
+++ api/views/updater.py
@@ -7,7 +7,7 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_200_OK
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 from shipper.models import Build, Device
 
@@ -17,7 +17,7 @@ def variant_check(variant):
         return Response(
             {
                 'message': "Wrong parameter. Try with the correct parameters."
-            }, status=HTTP_404_NOT_FOUND
+            }, status=HTTP_400_BAD_REQUEST
         )
 
 

41c6a9453680c192eae42edef14acbabb9ed31f3
Eric Park <me@ericswpark.com>
2021-07-13 00:56:22 +0900

    Move UserAdmin to accounts app

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from shipper.models import Device
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'is_active', 'email', 'last_login', 'get_devices', 'is_staff', 'is_superuser']
+    ordering = ['-is_active', '-last_login']
+
+    def get_devices(self, obj):
+        return [device.codename for device in Device.objects.filter(maintainers=obj)]
+
+    get_devices.short_description = 'Devices'
+
+
+admin.site.register(User, UserAdmin)
--- shipper/admin.py
+++ shipper/admin.py
@@ -1,20 +1,6 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin
 
-from .models import *
-
-User = get_user_model()
-
-
-class CustomUserAdmin(UserAdmin):
-    list_display = ['username', 'is_active', 'email', 'last_login', 'get_devices', 'is_staff', 'is_superuser']
-    ordering = ['-is_active', '-last_login']
-
-    def get_devices(self, obj):
-        return [device.codename for device in Device.objects.filter(maintainers=obj)]
-
-    get_devices.short_description = 'Devices'
+from .models import Device, Build, MirrorServer, Statistics
 
 
 class DeviceAdmin(admin.ModelAdmin):
@@ -73,9 +59,6 @@ class StatisticsAdmin(admin.ModelAdmin):
     list_display = ['date', 'download_count']
     ordering = ['date']
 
-
-admin.site.unregister(User)
-admin.site.register(User, CustomUserAdmin)
 
 admin.site.register(Device, DeviceAdmin)
 admin.site.register(Build, BuildAdmin)

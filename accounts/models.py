3e4e287ebcd8ba5402b7244f8198996242d8cb5a
Eric Park <me@ericswpark.com>
2021-07-13 01:35:42 +0900

    accounts: models: re-enable custom user model

--- accounts/models.py
+++ accounts/models.py
@@ -2,9 +2,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
-# class User(AbstractUser):
-#     class Meta:
-#         db_table = 'auth_user'
+class User(AbstractUser):
+    class Meta:
+        db_table = 'auth_user'
 
 
 class FakeModel(models.Model):

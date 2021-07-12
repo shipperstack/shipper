e9fe369c654594e44999e3ffe299bdf5ce8f06d3
Eric Park <me@ericswpark.com>
2021-07-13 01:50:22 +0900

    accounts: models: remove fake model
    Thanks for the fake migration

--- accounts/models.py
+++ accounts/models.py
@@ -1,11 +1,6 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 
 class User(AbstractUser):
     class Meta:
         db_table = 'auth_user'
-
-
-class FakeModel(models.Model):
-    pass

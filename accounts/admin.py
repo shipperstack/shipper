976a10e15b1b0b5df68b12e082b7afaf6eefa571
Eric Park <me@ericswpark.com>
2021-07-13 15:05:27 +0900

    accounts: admin: show full name as column

--- accounts/admin.py
+++ accounts/admin.py
@@ -8,8 +8,13 @@ User = get_user_model()
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ['username', 'is_active', 'email', 'last_login', 'get_devices', 'is_staff', 'is_superuser']
+    list_display = ['username', 'is_active', 'get_full_name', 'email', 'last_login', 'get_devices', 'is_staff',
+                    'is_superuser']
     ordering = ['-is_active', '-last_login']
+
+    # noinspection PyMethodMayBeStatic
+    def get_full_name(self, obj):
+        return "{} {}".format(obj.first_name, obj.last_name)
 
     def get_devices(self, obj):
         return [device.codename for device in Device.objects.filter(maintainers=obj)]

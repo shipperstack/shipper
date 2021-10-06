659ca68edd0fdc54e02ca75ca9989adb9569edfe
Eric Park <me@ericswpark.com>
2021-10-06 12:19:14 +0900

    entrypoint: double-quote to prevent globbing

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ if [ "$SHIPPER_SQL_ENGINE" = "django.db.backends.postgresql" ]
 then
     echo "Waiting for postgres..."
 
-    while ! nc -z $SHIPPER_SQL_HOST $SHIPPER_SQL_PORT; do
+    while ! nc -z "$SHIPPER_SQL_HOST" "$SHIPPER_SQL_PORT"; do
       sleep 0.1
       echo "Still waiting... If you see this multiple times then we can't find postgresql."
     done

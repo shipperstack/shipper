f5af0d47e369c4e5dd3d5f1152215571e44ce037
Eric Park <me@ericswpark.com>
2021-10-06 11:56:16 +0900

    downloads: use templatetag for rendering variant sections

--- downloads/templatetags/build_extras.py
+++ downloads/templatetags/build_extras.py
@@ -7,3 +7,12 @@ register = template.Library()
 def format_download_url(value, arg):
     """Formats download URL with file name"""
     return value.format(arg)
+
+
+@register.inclusion_tag('downloads_device_variant.html')
+def device_variant_section(device, variant, variant_name):
+    return {
+        'device': device,
+        'variant_name': variant_name,
+        'build_objects': device.get_all_build_objects_of_variant(variant=variant)
+    }

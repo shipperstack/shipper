616ecca7d947e557b086a210152b040f75221a96
Eric Park <me@ericswpark.com>
2021-10-06 11:45:38 +0900

    downloads: templatetags: build_extras: fix filter registration annotation

--- downloads/templatetags/build_extras.py
+++ downloads/templatetags/build_extras.py
@@ -3,7 +3,7 @@ from django import template
 register = template.Library()
 
 
-@register.simple_tag
+@register.filter
 def format_download_url(value, arg):
     """Formats download URL with file name"""
     return value.format(arg)

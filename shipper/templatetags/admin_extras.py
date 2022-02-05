89dd505ff6d3a3517e6cd51fd9679ed94e877713
Eric Park <me@ericswpark.com>
2022-02-05 06:01:14 +0000

    shipper: allow for multiple variants in admin stats page

---
+++ shipper/templatetags/admin_extras.py
@@ -0,0 +1,17 @@
+from django import template
+
+from config import settings
+
+from shipper.views import get_total_size
+from shipper.models import Build
+
+register = template.Library()
+
+
+@register.inclusion_tag('admin_stats_variant_row.html')
+def admin_stats_variant_row(variant):
+    return {
+        'variant_name': settings.SHIPPER_UPLOAD_VARIANTS[variant],
+        'variant_builds_count': Build.objects.filter(variant=variant).count(),
+        'variant_builds_size': get_total_size(Build.objects.filter(variant=variant)),
+    }
--- shipper/views/admin.py
+++ shipper/views/admin.py
@@ -26,15 +26,7 @@ class AdminStatisticsView(PermissionRequiredMixin, TemplateView):
         devices = Device.objects.all()
         maintainers = User.objects.all()
         builds = Build.objects.all()
-        vanilla_builds = builds.filter(variant="vanilla")
-        gapps_builds = builds.filter(variant="gapps")
-        foss_builds = builds.filter(variant="foss")
-        goapps_builds = builds.filter(variant="goapps")
         builds_size = get_total_size(builds)
-        vanilla_builds_size = get_total_size(vanilla_builds)
-        gapps_builds_size = get_total_size(gapps_builds)
-        foss_builds_size = get_total_size(foss_builds)
-        goapps_builds_size = get_total_size(goapps_builds)
 
         data = {
             'enabled_devices_count': devices.filter(status=True).count(),
@@ -42,15 +34,7 @@ class AdminStatisticsView(PermissionRequiredMixin, TemplateView):
             'disabled_devices_count': devices.filter(status=False).count(),
             'inactive_maintainers_count': maintainers.filter(is_active=False).count(),
             'builds_count': builds.count(),
-            'vanilla_builds_count': vanilla_builds.count(),
-            'gapps_builds_count': gapps_builds.count(),
-            'foss_builds_count': foss_builds.count(),
-            'goapps_builds_count': goapps_builds.count(),
             'builds_size': humanize.naturalsize(builds_size),
-            'vanilla_builds_size': humanize.naturalsize(vanilla_builds_size),
-            'gapps_builds_size': humanize.naturalsize(gapps_builds_size),
-            'foss_builds_size': humanize.naturalsize(foss_builds_size),
-            'goapps_builds_size': humanize.naturalsize(goapps_builds_size)
         }
 
         return render(request, self.template_name, data)

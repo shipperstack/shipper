544e7632d71becfaa7b3bd84d6cc26f98d3bae09
Eric Park <me@ericswpark.com>
2022-02-05 05:08:26 +0000

    shipper: management: commands: mirror_builds: fix DB query syntax
    Also use f-strings

--- shipper/management/commands/mirror_builds.py
+++ shipper/management/commands/mirror_builds.py
@@ -8,9 +8,9 @@ class Command(BaseCommand):
     help = "Mirrors builds that haven't been mirrored yet."
 
     def handle(self, *args, **options):
-        for build in Build.objects.exclude(is_mirrored=True):
-            self.stdout.write("Backing up build {}...".format(build.file_name))
-            mirror_build.delay(build.id)
-            self.stdout.write("Queued backup for build {}!".format(build.file_name))
+        for build in Build.objects.all():
+            if not build.is_mirrored():
+                mirror_build.delay(build.id)
+                self.stdout.write(f"Queued backup for build {build.file_name}!")
 
         self.stdout.write("Queued all incomplete builds. Please check the admin panel for status updates.")

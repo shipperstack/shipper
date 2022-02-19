ef468a07917763f87949cf5acfa4de30fa606054
Eric Park <me@ericswpark.com>
2022-02-19 02:29:43 +0000

    shipper: utils: add regex parse utility

--- shipper/utils.py
+++ shipper/utils.py
@@ -1,4 +1,8 @@
+import re
+
 from fnmatch import fnmatch
+
+from config import settings
 
 def is_version_in_target_versions(version, target_versions):
     if target_versions == "":
@@ -17,3 +21,13 @@ def is_version_in_target_versions(version, target_versions):
             if version == target_version:
                 return True
     return False
+
+
+def parse_filename_with_regex(filename):
+    pattern = re.compile(settings.SHIPPER_FILE_NAME_FORMAT)
+    m = p.search(filename)
+
+    if not m:
+        raise RegexParseException('invalid_file_name')
+
+    return {'version': m.group("version"), 'codename': m.group("codename"), 'variant': m.group("variant"), 'date': m.group("date")}

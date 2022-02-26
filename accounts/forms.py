c76a8426e773f1c35c0b7877a095a52c0680ed6a
Eric Park <me@ericswpark.com>
2022-02-26 14:22:37 +0000

    accounts: forms: use gettext_lazy

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from django.utils.translation import gettext
+from django.utils.translation import gettext_lazy
 from django import forms
 
 User = get_user_model()
@@ -11,10 +11,10 @@ class RegisterForm(forms.ModelForm):
 
         for field in self.Meta.required:
             self.fields[field].required = True
-            self.fields[field].help_text = gettext("Required.")
+            self.fields[field].help_text = gettext_lazy("Required.")
 
-    password = forms.CharField(label=gettext('Password'), widget=forms.PasswordInput)
-    password_verify = forms.CharField(label=gettext('Verify Password'), widget=forms.PasswordInput)
+    password = forms.CharField(label=gettext_lazy('Password'), widget=forms.PasswordInput)
+    password_verify = forms.CharField(label=gettext_lazy('Verify Password'), widget=forms.PasswordInput)
 
     class Meta:
         model = User
@@ -24,10 +24,10 @@ class RegisterForm(forms.ModelForm):
     def clean_password_verify(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password_verify']:
-            raise forms.ValidationError(gettext('The passwords do not match!'))
+            raise forms.ValidationError(gettext_lazy('The passwords do not match!'))
         return cd['password_verify']
 
     def clean_email(self):
         if User.objects.filter(email=self.cleaned_data['email']).exists():
-            raise forms.ValidationError(gettext('This email address is already registered and cannot be used!'))
+            raise forms.ValidationError(gettext_lazy('This email address is already registered and cannot be used!'))
         return self.cleaned_data['email']

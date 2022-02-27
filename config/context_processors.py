c74ff25d232cf07ecb2d372eb3d71af07c039e7e
Eric Park <me@ericswpark.com>
2022-02-27 05:49:02 +0000

    config: context_processors: flake8 edit

--- config/context_processors.py
+++ config/context_processors.py
@@ -3,17 +3,17 @@ from django.conf import settings
 
 def download_page_processor(_):
     return {
-            'main_website_url': settings.SHIPPER_MAIN_WEBSITE_URL,
-            'downloads_page_main_branding': settings.SHIPPER_DOWNLOADS_PAGE_MAIN_BRANDING,
-            'downloads_page_donation_url': settings.SHIPPER_DOWNLOADS_PAGE_DONATION_URL,
-            'downloads_page_donation_message': settings.SHIPPER_DOWNLOADS_PAGE_DONATION_MESSAGE,
-            'upload_variants': settings.SHIPPER_UPLOAD_VARIANTS,
+        "main_website_url": settings.SHIPPER_MAIN_WEBSITE_URL,
+        "downloads_page_main_branding": settings.SHIPPER_DOWNLOADS_PAGE_MAIN_BRANDING,
+        "downloads_page_donation_url": settings.SHIPPER_DOWNLOADS_PAGE_DONATION_URL,
+        "downloads_page_donation_message": settings.SHIPPER_DOWNLOADS_PAGE_DONATION_MESSAGE,  # noqa: E501
+        "upload_variants": settings.SHIPPER_UPLOAD_VARIANTS,
     }
 
 
 def version_processor(_):
-    return {'SHIPPER_VERSION': settings.SHIPPER_VERSION}
+    return {"SHIPPER_VERSION": settings.SHIPPER_VERSION}
 
 
 def debug_mode_processor(_):
-    return {'DEBUG': settings.DEBUG}
+    return {"DEBUG": settings.DEBUG}

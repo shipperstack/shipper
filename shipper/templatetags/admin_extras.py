768bcd2b9282685500db57ce86864d04edae0486
Eric Park <me@ericswpark.com>
2022-02-27 05:42:14 +0000

    shipper: templatetags: admin_extras: reformat document

--- shipper/templatetags/admin_extras.py
+++ shipper/templatetags/admin_extras.py
@@ -1,17 +1,17 @@
 from django import template
-
 from django.conf import settings
-
-from shipper.views import get_humanized_total_size
 from shipper.models import Build
+from shipper.views import get_humanized_total_size
 
 register = template.Library()
 
 
-@register.inclusion_tag('admin_stats_variant_row.html')
+@register.inclusion_tag("admin_stats_variant_row.html")
 def admin_stats_variant_row(variant):
     return {
-        'variant_name': settings.SHIPPER_UPLOAD_VARIANTS[variant],
-        'variant_builds_count': Build.objects.filter(variant=variant).count(),
-        'variant_builds_size': get_humanized_total_size(Build.objects.filter(variant=variant)),
+        "variant_name": settings.SHIPPER_UPLOAD_VARIANTS[variant],
+        "variant_builds_count": Build.objects.filter(variant=variant).count(),
+        "variant_builds_size": get_humanized_total_size(
+            Build.objects.filter(variant=variant)
+        ),
     }

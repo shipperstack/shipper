5a45c44c5db265ca5a0ebe55779d667390800289
Eric Park <me@ericswpark.com>
2022-02-27 04:40:04 +0000

    shipper: management: commands: format all files

--- shipper/management/commands/deinit_full_user.py
+++ shipper/management/commands/deinit_full_user.py
@@ -10,25 +10,33 @@ class Command(BaseCommand):
     help = "De-initializes a full user by removing access to all devices."
 
     def add_arguments(self, parser):
-        parser.add_argument('username', nargs=1, action='store', help='Username of user to be de-init')
+        parser.add_argument(
+            "username", nargs=1, action="store", help="Username of user to be de-init"
+        )
 
     def handle(self, *args, **options):
-        self.stdout.write("De-initializing user {}. Do you want to continue? [y/N] ".format(options['username'][0]),
-                          ending='')
+        self.stdout.write(
+            "De-initializing user {}. Do you want to continue? [y/N] ".format(
+                options["username"][0]
+            ),
+            ending="",
+        )
 
-        if input().lower() == 'y':
+        if input().lower() == "y":
             self.stdout.write("Starting removal...")
 
             # Make sure the user exists within the system
             try:
-                user = User.objects.get(username=options['username'][0])
+                user = User.objects.get(username=options["username"][0])
             except User.DoesNotExist:
-                self.stdout.write(self.style.ERROR("The specified user does not exist!"))
+                self.stdout.write(
+                    self.style.ERROR("The specified user does not exist!")
+                )
                 return
 
             # Remove user from all existing devices
             for device in Device.objects.all():
-                self.stdout.write("Removing device {}... ".format(device), ending='')
+                self.stdout.write("Removing device {}... ".format(device), ending="")
 
                 # Check if user is already a maintainer
                 if user not in device.maintainers.all():
--- shipper/management/commands/finalize_build_hashes.py
+++ shipper/management/commands/finalize_build_hashes.py
@@ -8,11 +8,17 @@ class Command(BaseCommand):
     help = "Calculates hashes for incomplete builds with interrupted processing."
 
     def handle(self, *args, **options):
-        builds = Build.objects.filter(sha256sum__exact='')
+        builds = Build.objects.filter(sha256sum__exact="")
 
         for build in builds:
-            self.stdout.write("Queueing re-calculation of SHA256 hash for build {}...".format(build.file_name))
+            self.stdout.write(
+                "Queueing re-calculation of SHA256 hash for build {}...".format(
+                    build.file_name
+                )
+            )
             generate_sha256.delay(build.id)
 
-        self.stdout.write("All incomplete builds have been queued for processing. Please check the admin panel for "
-                          "status updates.")
+        self.stdout.write(
+            "All incomplete builds have been queued for processing. Please check the "
+            "admin panel for status updates."
+        )

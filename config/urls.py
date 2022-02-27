f163618b87b4e3f39dc576aafb3668738a6804f6
Eric Park <me@ericswpark.com>
2022-02-27 05:49:38 +0000

    config: urls: reformat document

--- config/urls.py
+++ config/urls.py
@@ -1,15 +1,15 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-    path('', include('downloads.urls')),
-    path('', include('shipper.urls')),
-    path('api/', include('api.urls')),
-    path('', include('django.conf.urls.i18n')),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("accounts.urls")),
+    path("", include("downloads.urls")),
+    path("", include("shipper.urls")),
+    path("api/", include("api.urls")),
+    path("", include("django.conf.urls.i18n")),
 ]
 
 if settings.DEBUG:

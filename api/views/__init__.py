67a852d2f406f3cc34ff934c5e256a72840c36c2
Eric Park <me@ericswpark.com>
2022-02-27 05:28:33 +0000

    api: views: init: use explicit imports

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,4 +1,34 @@
-from .shippy import *
-from .statistics import *
-from .updater import *
-from .general import *
+from .general import V1GeneralBuildLatest, V1GeneralDeviceAll
+from .shippy import (
+    V1MaintainersChunkedUpload,
+    v1_maintainers_build_enabled_status_modify,
+    v1_maintainers_login,
+    v1_maintainers_token_check,
+    v1_maintainers_upload_filename_regex_pattern,
+    v1_system_info,
+)
+from .statistics import (
+    V1DownloadBuildCounter,
+    v1_download_count_all,
+    v1_download_count_day,
+    v1_download_count_month,
+    v1_download_count_week,
+)
+from .updater import V1UpdaterLOS
+
+__all__ = [
+    "V1GeneralDeviceAll",
+    "V1GeneralBuildLatest",
+    "V1MaintainersChunkedUpload",
+    "v1_maintainers_build_enabled_status_modify",
+    "v1_maintainers_login",
+    "v1_maintainers_token_check",
+    "v1_maintainers_upload_filename_regex_pattern",
+    "v1_system_info",
+    "V1DownloadBuildCounter",
+    "v1_download_count_day",
+    "v1_download_count_week",
+    "v1_download_count_month",
+    "v1_download_count_all",
+    "V1UpdaterLOS",
+]

6462a342afbef73ea719bf01d531a49240c40a8c
Eric Park <me@ericswpark.com>
2022-02-27 04:56:54 +0000

    shipper: views: init: explicit import statements

--- shipper/views/__init__.py
+++ shipper/views/__init__.py
@@ -1,2 +1,15 @@
-from .maintainers import *
-from .admin import *
+from .admin import AdminStatisticsView
+from .maintainers import (
+    BuildDeleteView,
+    DeviceDetailView,
+    MaintainerDashboardView,
+    build_enabled_status_modify,
+)
+
+__all__ = [
+    "MaintainerDashboardView",
+    "DeviceDetailView",
+    "BuildDeleteView",
+    "build_enabled_status_modify",
+    "AdminStatisticsView",
+]

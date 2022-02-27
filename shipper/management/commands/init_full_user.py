4142bbc92a3b76c2ddf71a9faaf8a970fbf0f5cd
Eric Park <me@ericswpark.com>
2022-02-27 04:39:03 +0000

    shipper: management: init_full_user: format document

--- shipper/management/commands/init_full_user.py
+++ shipper/management/commands/init_full_user.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
 from django.core.management import BaseCommand
-
 from shipper.models import Device
 
 User = get_user_model()
@@ -10,27 +9,35 @@ class Command(BaseCommand):
     help = "Initializes a user with access to all enabled devices."
 
     def add_arguments(self, parser):
-        parser.add_argument('username', nargs=1, action='store', help='Username of user to be init')
+        parser.add_argument(
+            "username", nargs=1, action="store", help="Username of user to be init"
+        )
 
     def handle(self, *args, **options):
-        self.stdout.write("Initializing user {}. Do you want to continue? [y/N] ".format(options['username'][0]),
-                          ending='')
+        self.stdout.write(
+            "Initializing user {}. Do you want to continue? [y/N] ".format(
+                options["username"][0]
+            ),
+            ending="",
+        )
 
-        if input().lower() == 'y':
+        if input().lower() == "y":
             self.stdout.write("Starting initialization...")
 
             # Make sure the user exists within the system
             try:
-                user = User.objects.get(username=options['username'][0])
+                user = User.objects.get(username=options["username"][0])
             except User.DoesNotExist:
-                self.stdout.write(self.style.ERROR("The specified user does not exist!"))
+                self.stdout.write(
+                    self.style.ERROR("The specified user does not exist!")
+                )
                 return
 
             # Add user to all existing devices
             devices = Device.objects.filter(status=True)
 
             for device in devices:
-                self.stdout.write("Adding device {}... ".format(device), ending='')
+                self.stdout.write("Adding device {}... ".format(device), ending="")
 
                 # Check if user is already a maintainer
                 if user in device.maintainers.all():
@@ -39,7 +46,9 @@ class Command(BaseCommand):
                     device.maintainers.add(user)
                     self.stdout.write(self.style.SUCCESS("Added"))
 
-            self.stdout.write("The specified user has been added to all enabled devices!")
+            self.stdout.write(
+                "The specified user has been added to all enabled devices!"
+            )
         else:
             self.stdout.write("Exiting...")
             return

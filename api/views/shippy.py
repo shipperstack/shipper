b203301314118942e7e5fb5502e8610c6f2c8c34
Eric Park <me@ericswpark.com>
2022-02-27 05:17:55 +0000

    api: views: shippy: format document

--- api/views/shippy.py
+++ api/views/shippy.py
@@ -1,30 +1,32 @@
 import os
 
+from django.conf import settings
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import update_last_login
-from django.conf import settings
 from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
-
 from drf_chunked_upload.serializers import ChunkedUploadSerializer
 from drf_chunked_upload.views import ChunkedUploadView
 from rest_framework.authtoken.models import Token
-from rest_framework.decorators import permission_classes, api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.status import HTTP_401_UNAUTHORIZED, HTTP_400_BAD_REQUEST, HTTP_200_OK, HTTP_404_NOT_FOUND
-
+from rest_framework.status import (
+    HTTP_200_OK,
+    HTTP_400_BAD_REQUEST,
+    HTTP_401_UNAUTHORIZED,
+    HTTP_404_NOT_FOUND,
+)
 from shipper.exceptions import *
 from shipper.handler import handle_chunked_build
-from shipper.models import Device, Build
+from shipper.models import Build, Device
 from shipper.utils import parse_filename_with_regex
 
-from django.conf import settings
 
 # Serializer for overriding success url
 class V1MaintainersChunkedUploadSerializer(ChunkedUploadSerializer):
     # noinspection SpellCheckingInspection
-    viewname = 'v1_maintainers_chunked_upload_detail'
+    viewname = "v1_maintainers_chunked_upload_detail"
 
 
 class V1MaintainersChunkedUpload(ChunkedUploadView):
@@ -35,16 +37,18 @@ class V1MaintainersChunkedUpload(ChunkedUploadView):
         Validates chunked upload and transfers to handler
         """
         try:
-            device_codename = parse_filename_with_regex(chunked_upload.filename)['codename']
+            device_codename = parse_filename_with_regex(chunked_upload.filename)[
+                "codename"
+            ]
             device = Device.objects.get(codename=device_codename)
         except (RegexParseException, Device.DoesNotExist):
             chunked_upload.delete()
             return Response(
                 {
-                    'error': 'unknown_device',
-                    'message': 'The specified device does not exist!'
+                    "error": "unknown_device",
+                    "message": "The specified device does not exist!",
                 },
-                status=HTTP_404_NOT_FOUND
+                status=HTTP_404_NOT_FOUND,
             )
 
         # Check if maintainer is in device's approved maintainers list
@@ -52,22 +56,21 @@ class V1MaintainersChunkedUpload(ChunkedUploadView):
             chunked_upload.delete()
             return Response(
                 {
-                    'error': 'insufficient_permissions',
-                    'message': 'You are not authorized to upload for this device!'
+                    "error": "insufficient_permissions",
+                    "message": "You are not authorized to upload for this device!",
                 },
-                status=HTTP_401_UNAUTHORIZED
+                status=HTTP_401_UNAUTHORIZED,
             )
 
         try:
-            build_id = handle_chunked_build(device, chunked_upload, request.POST.get('md5'))
+            build_id = handle_chunked_build(
+                device, chunked_upload, request.POST.get("md5")
+            )
         except (UploadException, RegexParseException) as exception:
             chunked_upload.delete()
             return Response(
-                {
-                    'error': str(exception),
-                    'message': exception_to_message(exception)
-                },
-                status=HTTP_400_BAD_REQUEST
+                {"error": str(exception), "message": exception_to_message(exception)},
+                status=HTTP_400_BAD_REQUEST,
             )
 
         # Upload was successful, update user's last login timestamp
@@ -75,10 +78,10 @@ class V1MaintainersChunkedUpload(ChunkedUploadView):
 
         return Response(
             {
-                'message': 'Build has been uploaded for device {}!'.format(device),
-                'build_id': build_id
+                "message": "Build has been uploaded for device {}!".format(device),
+                "build_id": build_id,
             },
-            status=HTTP_200_OK
+            status=HTTP_200_OK,
         )
 
 
@@ -91,18 +94,19 @@ def v1_maintainers_login(request):
     if username is None or password is None:
         return Response(
             {
-                'error': 'blank_username_or_password',
-                'message': 'Username or password cannot be blank!'
+                "error": "blank_username_or_password",
+                "message": "Username or password cannot be blank!",
             },
-            status=HTTP_400_BAD_REQUEST
+            status=HTTP_400_BAD_REQUEST,
         )
     user = authenticate(username=username, password=password)
     if not user:
-        return Response({
-            'error': 'invalid_credential',
-            'message': 'Invalid credentials. Please try again.'
-        },
-            status=HTTP_404_NOT_FOUND
+        return Response(
+            {
+                "error": "invalid_credential",
+                "message": "Invalid credentials. Please try again.",
+            },
+            status=HTTP_404_NOT_FOUND,
         )
 
     # Update login timestamp
@@ -111,11 +115,7 @@ def v1_maintainers_login(request):
     # Generate and return token
     token, _ = Token.objects.get_or_create(user=user)
     return Response(
-        {
-            'token': token.key,
-            'message': 'Successfully logged in!'
-        },
-        status=HTTP_200_OK
+        {"token": token.key, "message": "Successfully logged in!"}, status=HTTP_200_OK
     )
 
 
@@ -123,11 +123,7 @@ def v1_maintainers_login(request):
 @api_view(["GET"])
 @permission_classes((AllowAny,))
 def v1_system_info(_):
-    return Response(
-        {
-            'version': settings.SHIPPER_VERSION
-        }
-    )
+    return Response({"version": settings.SHIPPER_VERSION})
 
 
 @csrf_exempt
@@ -136,12 +132,7 @@ def v1_maintainers_token_check(request):
     # Update login timestamp
     update_last_login(None, request.user)
 
-    return Response(
-        {
-            'username': request.user.username
-        },
-        status=HTTP_200_OK
-    )
+    return Response({"username": request.user.username}, status=HTTP_200_OK)
 
 
 @csrf_exempt
@@ -150,12 +141,7 @@ def v1_maintainers_upload_filename_regex_pattern(request):
     # Update login timestamp
     update_last_login(None, request.user)
 
-    return Response(
-        {
-            'pattern': settings.SHIPPER_FILE_NAME_FORMAT
-        },
-        status=HTTP_200_OK
-    )
+    return Response({"pattern": settings.SHIPPER_FILE_NAME_FORMAT}, status=HTTP_200_OK)
 
 
 @csrf_exempt
@@ -166,11 +152,11 @@ def v1_maintainers_build_enabled_status_modify(request):
     if build_id is None or enable is None:
         return Response(
             {
-                'error': 'missing_parameters',
-                'message': 'One or more of the required parameters is blank! Required parameters: build ID, '
-                           'enabled flag'
+                "error": "missing_parameters",
+                "message": "One or more of the required parameters is blank! Required parameters: build ID, "
+                "enabled flag",
             },
-            status=HTTP_400_BAD_REQUEST
+            status=HTTP_400_BAD_REQUEST,
         )
 
     build = get_object_or_404(Build, pk=build_id)
@@ -180,10 +166,10 @@ def v1_maintainers_build_enabled_status_modify(request):
     if request.user not in build.device.maintainers.all():
         return Response(
             {
-                'error': 'insufficient_permissions',
-                'message': 'You are not authorized to modify builds associated with this device!'
+                "error": "insufficient_permissions",
+                "message": "You are not authorized to modify builds associated with this device!",
             },
-            status=HTTP_401_UNAUTHORIZED
+            status=HTTP_401_UNAUTHORIZED,
         )
 
     # Switch build status
@@ -195,30 +181,24 @@ def v1_maintainers_build_enabled_status_modify(request):
 
     if enable:
         return Response(
-            {
-                'message': 'Successfully enabled the build!'
-            },
-            status=HTTP_200_OK
+            {"message": "Successfully enabled the build!"}, status=HTTP_200_OK
         )
     else:
         return Response(
-            {
-                'message': 'Successfully disabled the build!'
-            },
-            status=HTTP_200_OK
+            {"message": "Successfully disabled the build!"}, status=HTTP_200_OK
         )
 
 
 def exception_to_message(e):
     e = str(e)
-    if e == 'file_name_mismatch':
+    if e == "file_name_mismatch":
         return "The file name does not match the checksum file name!"
-    if e == 'invalid_file_name':
+    if e == "invalid_file_name":
         return "The file name was malformed. Please do not edit the file name!"
-    if e == 'not_official':
+    if e == "not_official":
         return "Only official builds are allowed."
-    if e == 'codename_mismatch':
+    if e == "codename_mismatch":
         return "The codename does not match the file!"
-    if e == 'duplicate_build':
+    if e == "duplicate_build":
         return "The build already exists in the system!"
     return "An unknown error occurred."

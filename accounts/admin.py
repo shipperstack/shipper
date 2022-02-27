407e11d82d856475486b4b6dd0a3844704b88584
Eric Park <me@ericswpark.com>
2022-02-27 05:46:13 +0000

    accounts: admin: reformat document

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,24 +1,33 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
-
 from shipper.models import Device
 
 User = get_user_model()
 
 
 class CustomUserAdmin(UserAdmin):
-    list_display = ['username', 'is_active', 'get_full_name', 'email', 'last_login', 'get_devices', 'is_staff',
-                    'is_superuser']
-    ordering = ['-is_active', '-last_login']
+    list_display = [
+        "username",
+        "is_active",
+        "get_full_name",
+        "email",
+        "last_login",
+        "get_devices",
+        "is_staff",
+        "is_superuser",
+    ]
+    ordering = ["-is_active", "-last_login"]
 
     def get_full_name(self, obj):
         return "{} {}".format(obj.first_name, obj.last_name)
-    get_full_name.short_description = 'Full Name'
+
+    get_full_name.short_description = "Full Name"
 
     def get_devices(self, obj):
         return [device.codename for device in Device.objects.filter(maintainers=obj)]
-    get_devices.short_description = 'Devices'
+
+    get_devices.short_description = "Devices"
 
 
 admin.site.register(User, CustomUserAdmin)

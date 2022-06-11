de73bf61550ea62e97a709b97e9ba1c37f4b5b73
Eric Park <me@ericswpark.com>
2022-06-11 15:41:22 +0900

    core: urls: remove duplicate calls to maintainer endpoints
    Forgot to remove them during the migration

--- core/urls.py
+++ core/urls.py
@@ -1,31 +1,7 @@
 from django.urls import path
 
-from .views import (
-    AdminStatisticsView,
-    BuildDeleteView,
-    DeviceDetailView,
-    MaintainerDashboardView,
-    build_enabled_status_modify,
-)
+from .views import AdminStatisticsView
 
 urlpatterns = [
-    # Maintainers
-    path(
-        "maintainers/", MaintainerDashboardView.as_view(), name="maintainer_dashboard"
-    ),
-    path(
-        "maintainers/device/<int:pk>/", DeviceDetailView.as_view(), name="device_detail"
-    ),
-    path(
-        "maintainers/build/<int:pk>/enabled_status_modify/",
-        build_enabled_status_modify,
-        name="build_enabled_status_modify",
-    ),
-    path(
-        "maintainers/build/<int:pk>/delete/",
-        BuildDeleteView.as_view(),
-        name="build_delete",
-    ),
-    # Admins
     path("admins/statistics/", AdminStatisticsView.as_view(), name="admin_statistics"),
 ]

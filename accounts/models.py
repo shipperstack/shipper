b12c0863459d20c8c972f16ba9e81eb0ce01e9d0
Eric Park <me@ericswpark.com>
2022-06-11 22:51:51 +0900

    accounts: add initial field to allow full access to all devices

--- accounts/models.py
+++ accounts/models.py
@@ -1,9 +1,10 @@
 from auditlog.registry import auditlog
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    full_access_to_devices = models.BooleanField(default=False, help_text="Has access to all devices on shipper")
 
 
 auditlog.register(User)

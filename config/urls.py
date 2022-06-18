f4ba816297fad53af0baa2c0e6d0ac62881350c7
Eric Park <me@ericswpark.com>
2022-06-18 12:23:15 +0900

    config: urls: move Django internal admin to django-admin/

--- config/urls.py
+++ config/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("django-admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
     path("maintainers/", include("maintainers.urls")),
     path("", include("downloads.urls")),

595b75199cb4ac1ffcb15ca070e774ceabc2693e
Eric Park <me@ericswpark.com>
2022-11-27 23:06:42 +0900

    config: settings: update BASE_DIR to Django 4.x defaults

--- config/settings.py
+++ config/settings.py
@@ -6,8 +6,10 @@ from paramiko import AuthenticationException, SSHException
 from billiard.exceptions import TimeLimitExceeded
 from sentry_sdk.integrations.django import DjangoIntegration
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+from pathlib import Path
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.environ.get("SHIPPER_SECRET_KEY")
 DEBUG = int(os.environ.get("SHIPPER_DEBUG", default=0))

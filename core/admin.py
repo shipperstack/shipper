c43371911a3d6aa19d16db815a26edb2aafccee8
Eric Park <me@ericswpark.com>
2022-11-30 21:52:39 +0900

    core: admin: remove missing fields from Statistics model

--- core/admin.py
+++ core/admin.py
@@ -71,8 +71,8 @@ class MirrorServerAdmin(admin.ModelAdmin):
 
 
 class StatisticsAdmin(admin.ModelAdmin):
-    readonly_fields = ["time", "device", "build", "ip"]
-    list_display = ["time", "device", "build", "ip"]
+    readonly_fields = ["time", "build", "ip"]
+    list_display = ["time", "build", "ip"]
     ordering = ["time"]
 
 

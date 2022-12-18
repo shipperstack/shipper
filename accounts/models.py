db848dd528d943528b1b65b063c83df8d2ba9ce5
Eric Park <me@ericswpark.com>
2022-12-18 23:52:42 +0900

    accounts: models: add more fields for users

--- accounts/models.py
+++ accounts/models.py
@@ -7,6 +7,20 @@ class User(AbstractUser):
     full_access_to_devices = models.BooleanField(
         default=False, help_text="Has access to all devices on shipper"
     )
+    profile_picture = models.URLField(
+        help_text="URL to profile picture.",
+        blank=True,
+    )
+    bio = models.TextField(
+        max_length=500,
+        help_text="Short bio about yourself!",
+        blank=True,
+    )
+    contact_url = models.URLField(
+        help_text="Where users should contact you.<br>Example: https://t.me/@example, "
+                  "mailto:john.appleseed@example.com ",
+        blank=True,
+    )
 
 
 auditlog.register(User)

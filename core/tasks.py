799bc1b0e724ff4209ec895bead36accd07162f4
Eric Park <me@ericswpark.com>
2023-03-29 22:55:32 +0900

    refactor: change variable name

--- core/tasks.py
+++ core/tasks.py
@@ -111,9 +111,9 @@ def upload_build_to_mirror(self, build_id, build, mirror, task_id):
     # Define callback for printing progress
     def update_progress(transferred, total):
         previous_result = AsyncResult(task_id)
-        previous_current = previous_result.info["current"]
+        previous_transferred = previous_result.info["current"]
 
-        if previous_current != transferred:
+        if previous_transferred != transferred:
             signal.alarm(SFTP_HANG_TIMEOUT)
 
         self.update_state(

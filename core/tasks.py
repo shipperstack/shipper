ec44fbcd3c02ecc4c2126df6ab2f5ead49e4acf9
Eric Park <me@ericswpark.com>
2023-03-30 23:12:33 +0900

    fix: set default value for fetching build mirror result

--- core/tasks.py
+++ core/tasks.py
@@ -110,7 +110,7 @@ def upload_build_to_mirror(self, build_id, build, mirror, task_id):
     # Define callback for printing progress
     def update_progress(transferred, total):
         previous_result = AsyncResult(task_id)
-        previous_transferred = int(previous_result.info.get("current"))
+        previous_transferred = int(previous_result.info.get("current", -1))
 
         if previous_transferred == transferred:
             logger.warning(f"SFTP seems to be hung. Currently at {transferred} bytes.")

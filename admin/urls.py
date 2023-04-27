109d6b7a9fe2adc5d9d7f073dea6870e8d9c1459
Eric Park <me@ericswpark.com>
2023-04-27 21:25:39 +0900

    refactor: change admin endpoint URL

--- admin/urls.py
+++ admin/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from admin.views import AdminStatisticsView
 
 urlpatterns = [
-    path("admins/statistics/", AdminStatisticsView.as_view(), name="admin_statistics"),
+    path("statistics/", AdminStatisticsView.as_view(), name="admin_statistics"),
 ]

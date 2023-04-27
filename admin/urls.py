2669d117aa51e97c54fd284c72da293c58791848
Eric Park <me@ericswpark.com>
2023-04-28 00:06:33 +0900

    feat: add view to see build mirror status

--- admin/urls.py
+++ admin/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from admin.views import AdminStatisticsView
+from admin.views import AdminStatisticsView, AdminBuildMirrorStatusView
 
 urlpatterns = [
     path("statistics/", AdminStatisticsView.as_view(), name="admin_statistics"),
+    path("mirror_status/", AdminBuildMirrorStatusView.as_view(), name="admin_build_mirror_status"),
 ]
--- admin/views.py
+++ admin/views.py
@@ -3,6 +3,8 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django_celery_results.models import TaskResult
+
 from core.models import Build, Device
 
 User = get_user_model()
@@ -35,3 +37,33 @@ class AdminStatisticsView(PermissionRequiredMixin, TemplateView):
         }
 
         return render(request, self.template_name, data)
+
+
+class AdminBuildMirrorStatusView(PermissionRequiredMixin, TemplateView):
+    permission_required = "is_staff"
+    template_name = "admin_build_mirror_status.html"
+
+    def get(self, request, *args, **kwargs):
+        fetch_limit = 100
+        raw_results = TaskResult.objects.filter(task_name="mirror_build")[:fetch_limit]
+        mirror_results = []
+
+        for raw_result in raw_results:
+            build = Build.objects.get(id=eval(raw_result.task_args))
+
+            mirror_results.append({
+                "build_name": build.file_name,
+                "status": raw_result.status,
+                "current": humanize.naturalsize(raw_result.result.current),
+                "total": humanize.naturalsize(raw_result.result.total),
+                "percent": int(raw_result.result.current * 100 / raw_result.result.total),
+            })
+
+        data = {
+            "mirror_results": mirror_results,
+            "fetch_limit": fetch_limit,
+        }
+
+        return render(request, self.template_name, data)
+
+

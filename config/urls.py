5bddc4229494c76573bdb1591dd54cc083037b22
Eric Park <me@ericswpark.com>
2023-07-14 12:35:14 +0900

    feat: add well-known password change redirect
    Closes #209

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
@@ -11,6 +12,10 @@ urlpatterns = [
     path("internal-admin/", include("internaladmin.urls")),
     path("api/", include("api.urls")),
     path("", include("django.conf.urls.i18n")),
+    path(
+        ".well-known/change-password/",
+        RedirectView.as_view(pattern_name="password_change", permanent=False),
+    ),
 ]
 
 if settings.DEBUG:

c53c8dd867e5a346f22772ae76613c234f41fccd
Eric Park <me@ericswpark.com>
2023-07-16 12:22:28 +0900

    feat: add new Dockerfile for backend server

---
+++ server/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+if [ "$SHIPPER_SQL_ENGINE" = "django.db.backends.postgresql" ]
+then
+    echo "Waiting for PostgreSQL"
+
+    while ! nc -z "$SHIPPER_SQL_HOST" "$SHIPPER_SQL_PORT"; do
+      sleep 0.1
+      echo "Still waiting on PostgreSQL"
+    done
+
+    echo "PostgreSQL started!"
+fi
+
+echo -e "Running command: $@"
+exec "$@"

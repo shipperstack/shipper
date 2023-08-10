927dedce59ca4f8a4a7833616113645a56d606ca
Eric Park <me@ericswpark.com>
2023-08-10 19:00:41 +0900

    style(server): reformat with prettier

--- server/front-end/downloads-main.js
+++ server/front-end/downloads-main.js
@@ -1,65 +1,84 @@
 import React from "react";
-import {createRoot} from "react-dom/client";
+import { createRoot } from "react-dom/client";
 
-const root = createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById("root"));
 
-const ACTIVE_DEVICES = JSON.parse(document.getElementById('active-devices').textContent);
+const ACTIVE_DEVICES = JSON.parse(
+  document.getElementById("active-devices").textContent,
+);
 
 function App() {
-    let [searchTerm, setSearchTerm] = React.useState("");
-    return (
-        <>
-            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
-            <DownloadList filter={searchTerm}/>
-        </>
-    )
+  let [searchTerm, setSearchTerm] = React.useState("");
+  return (
+    <>
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      <DownloadList filter={searchTerm} />
+    </>
+  );
 }
 
-function SearchBar({searchTerm, setSearchTerm}) {
-    function changedInput(event) {
-        setSearchTerm(event.target.value);
+function SearchBar({ searchTerm, setSearchTerm }) {
+  function changedInput(event) {
+    setSearchTerm(event.target.value);
+  }
+
+  return (
+    <input
+      className="form-control"
+      type="text"
+      placeholder="Search for devices"
+      value={searchTerm}
+      onChange={changedInput}
+    />
+  );
+}
+
+function DownloadList({ filter }) {
+  let filtered_devices = [];
+
+  ACTIVE_DEVICES.forEach((element) => {
+    if (filter === "" || element.name.includes(filter)) {
+      filtered_devices.push(element);
     }
+  });
 
-    return (<input className="form-control" type="text" placeholder="Search for devices" value={searchTerm}
-                   onChange={changedInput}/>);
-}
-
-function DownloadList({filter}) {
-    let filtered_devices = [];
-
-    ACTIVE_DEVICES.forEach((element) => {
-        if (filter === "" || element.name.includes(filter)) {
-            filtered_devices.push(element);
-        }
-    })
-
-    return (
-        <div className="container" style={{marginTop: "5px", marginBottom: "30px"}}>
-            <div className="row row-cols-1 row-cols-md-4 g-4">
-                {filtered_devices.map(device => {
-                    let device_card_style = device.enabled ? "col" : "col disabled-device-card";
-                    let device_photo_url = device.photo_url === "" ? "static/img/no_device_image.png" : device.photo_url;
-                    let device_card_title_style = device.enabled ? "card-title" : "card-title text-reset text-decoration-line-through";
-                    return (
-                        <div className={device_card_style} key={device.codename}>
-                            <a className="card-url" href={device.url}>
-                                <div className="card h-100">
-                                    <img src={device_photo_url}
-                                         className="card-img-top"></img>
-                                    <div className="card-body">
-                                        <h5 className={device_card_title_style}>{device.name}</h5>
-                                    </div>
-                                </div>
-                            </a>
-                        </div>
-                    );
-                })}
+  return (
+    <div
+      className="container"
+      style={{ marginTop: "5px", marginBottom: "30px" }}
+    >
+      <div className="row row-cols-1 row-cols-md-4 g-4">
+        {filtered_devices.map((device) => {
+          let device_card_style = device.enabled
+            ? "col"
+            : "col disabled-device-card";
+          let device_photo_url =
+            device.photo_url === ""
+              ? "static/img/no_device_image.png"
+              : device.photo_url;
+          let device_card_title_style = device.enabled
+            ? "card-title"
+            : "card-title text-reset text-decoration-line-through";
+          return (
+            <div className={device_card_style} key={device.codename}>
+              <a className="card-url" href={device.url}>
+                <div className="card h-100">
+                  <img src={device_photo_url} className="card-img-top"></img>
+                  <div className="card-body">
+                    <h5 className={device_card_title_style}>{device.name}</h5>
+                  </div>
+                </div>
+              </a>
             </div>
-        </div>
-    );
+          );
+        })}
+      </div>
+    </div>
+  );
 }
-
 
 root.render(
-    <React.StrictMode>
-    <App/></React.StrictMode>);
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);

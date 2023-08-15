081b1b190a90763d3e6c2ff03f81750bc0e94750
Eric Park <me@ericswpark.com>
2023-08-15 20:15:47 +0900

    feat(server): add latest API endpoints (unstable!)
    These endpoints are unstable! Only use them if you don't like sleeping
    whenever I push a new update.

--- server/api/urls.py
+++ server/api/urls.py
@@ -23,6 +23,31 @@ from .views import (
 urlpatterns = [
     # statistics
     path(
+        "latest/download/build/counter/",
+        V2DownloadBuildCounter.as_view(),
+        name="latest_download_build_counter",
+    ),
+    path(
+        "latest/download/count/day/",
+        v1_download_count_day,
+        name="latest_download_count_day",
+    ),
+    path(
+        "latest/download/count/week/",
+        v1_download_count_week,
+        name="latest_download_count_week",
+    ),
+    path(
+        "latest/download/count/month/",
+        v1_download_count_month,
+        name="latest_download_count_month",
+    ),
+    path(
+        "latest/download/count/all/",
+        v1_download_count_all,
+        name="latest_download_count_all",
+    ),
+    path(
         "v1/download/build/counter/",
         V1DownloadBuildCounter.as_view(),
         name="v1_download_build_counter",
@@ -43,6 +68,37 @@ urlpatterns = [
     ),
     path("v1/download/count/all/", v1_download_count_all, name="v1_download_count_all"),
     # shippy
+    path("latest/system/info/", v1_system_info, name="latest_system_info"),
+    path(
+        "latest/maintainers/login/",
+        v1_maintainers_login,
+        name="latest_maintainers_login",
+    ),
+    path(
+        "latest/maintainers/token_check/",
+        v1_maintainers_token_check,
+        name="latest_maintainers_token_check",
+    ),
+    path(
+        "latest/maintainers/upload_filename_regex_pattern",
+        v1_maintainers_upload_filename_regex_pattern,
+        name="latest_maintainers_upload_filename_regex_pattern",
+    ),
+    path(
+        "latest/maintainers/chunked_upload/",
+        V1MaintainersChunkedUpload.as_view(),
+        name="latest_maintainers_chunked_upload",
+    ),
+    path(
+        "latest/maintainers/chunked_upload/<uuid:pk>/",
+        V1MaintainersChunkedUpload.as_view(),
+        name="latest_maintainers_chunked_upload_detail",
+    ),
+    path(
+        "latest/maintainers/build/enabled_status_modify/",
+        v1_maintainers_build_enabled_status_modify,
+        name="latest_maintainers_build_enabled_status_modify",
+    ),
     path("v1/system/info/", v1_system_info, name="v1_system_info"),
     path("v1/maintainers/login/", v1_maintainers_login, name="v1_maintainers_login"),
     path(
@@ -72,11 +128,36 @@ urlpatterns = [
     ),
     # updater
     path(
+        "latest/updater/los/<slug:codename>/<slug:variant>/",
+        V1UpdaterLOS.as_view(),
+        name="latest_updater_los",
+    ),
+    path(
         "v1/updater/los/<slug:codename>/<slug:variant>/",
         V1UpdaterLOS.as_view(),
         name="v1_updater_los",
     ),
     # general
+    path(
+        "latest/general/device/all/",
+        V1GeneralDeviceAll.as_view(),
+        name="latest_general_device_all",
+    ),
+    path(
+        "latest/general/maintainer/all/",
+        V1GeneralMaintainerAll.as_view(),
+        name="latest_general_maintainer_all",
+    ),
+    path(
+        "latest/general/maintainer/active/",
+        V1GeneralMaintainerActive.as_view(),
+        name="latest_general_maintainer_active",
+    ),
+    path(
+        "latest/general/build/latest/<slug:codename>/<slug:variant>/",
+        V1GeneralBuildLatest.as_view(),
+        name="latest_general_build_latest",
+    ),
     path(
         "v1/general/device/all/",
         V1GeneralDeviceAll.as_view(),

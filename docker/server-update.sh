5882c647c37760101b7e21875239047d6dfda0fd
Eric Park <me@ericswpark.com>
2024-04-02 15:51:36 -0400

    docker: update server-update script to use new Docker Compose cmd

--- docker/server-update.sh
+++ docker/server-update.sh
@@ -14,12 +14,12 @@ fi
 # Execute commands based on developer flag
 if [ "$developer" = true ]; then
     echo "Using developer docker-compose file..."
-    docker-compose -f docker-compose.dev.yml exec web python3 manage.py migrate --noinput
-    docker-compose -f docker-compose.dev.yml exec web python3 manage.py compilemessages
-    docker-compose -f docker-compose.dev.yml exec web python3 manage.py collectstatic --no-input --clear
+    docker compose -f docker-compose.dev.yml exec web python3 manage.py migrate --noinput
+    docker compose -f docker-compose.dev.yml exec web python3 manage.py compilemessages
+    docker compose -f docker-compose.dev.yml exec web python3 manage.py collectstatic --no-input --clear
 else
     echo "Using production docker-compose file..."
-    docker-compose exec web python3 manage.py migrate --noinput
-    docker-compose exec web python3 manage.py compilemessages
-    docker-compose exec web python3 manage.py collectstatic --no-input --clear
+    docker compose exec web python3 manage.py migrate --noinput
+    docker compose exec web python3 manage.py compilemessages
+    docker compose exec web python3 manage.py collectstatic --no-input --clear
 fi

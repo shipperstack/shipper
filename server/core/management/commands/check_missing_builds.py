e5ce1686bb3fabdeac8251dff209c01940103044
Eric Park <me@ericswpark.com>
2024-04-02 16:38:04 -0400

    fix(server): add command to unset missing build files

---
+++ server/core/management/commands/check_missing_builds.py
@@ -0,0 +1,16 @@
+from django.core.management import BaseCommand
+from core.models import Build
+
+class Command(BaseCommand):
+    help = "Checks if build files exist on the server and unsets the build file field if not."
+
+    def handle(self, *args, **options):
+        for build in Build.objects.all():
+            if bool(build.zip_file) and build.zip_file.storage.exists(build.zip_file):
+                self.stdout.write(f"Build {build.file_name} is available in the system, not unsetting.")
+            else:
+                self.stdout.write(f"Build {build.file_name} does not exist, unsetting... ", ending='')
+                build.zip_file.delete(save=True)
+                self.stdout.write(f"Unset.")
+
+        self.stdout.write("All missing builds have been cleared from the system.")

c722c11207c6fff500e584384a4669e2b63f3c22
Eric Park <me@ericswpark.com>
2024-04-02 19:44:29 -0400

    fix(server): fix checking build file existence

--- server/core/management/commands/check_missing_builds.py
+++ server/core/management/commands/check_missing_builds.py
@@ -6,7 +6,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for build in Build.objects.all():
-            if bool(build.zip_file) and build.zip_file.storage.exists(build.zip_file):
+            if bool(build.zip_file) and build.zip_file.storage.exists(build.zip_file.name):
                 self.stdout.write(f"Build {build.file_name} is available in the system, not unsetting.")
             else:
                 self.stdout.write(f"Build {build.file_name} does not exist, unsetting... ", ending='')

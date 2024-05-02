13b28c8820c24151fcb53e2b9d0a8dd4d7658214
Eric Park <me@ericswpark.com>
2024-05-02 00:49:30 -0400

    test(server): fix download URL construction test

--- server/downloads/tests.py
+++ server/downloads/tests.py
@@ -11,7 +11,7 @@ class ShipperTemplateTagsTestCase(TestCase):
     def test_format_download_url(self):
         self.assertEqual(
             "https://mock/test/Bliss-v14.zip/download/",
-            format_download_url("https://mock/test/{}/download/", "Bliss-v14.zip"),
+            format_download_url("https://mock/test/{}/download/", "Bliss-v14"),
         )
 
 

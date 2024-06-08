4b624b97b6d196e8007bb9cb53638898d0abbb68
Eric Park <me@ericswpark.com>
2024-06-08 15:01:13 +0900

    feat(server): add checkbox to unfilter disabled devices (#329)
    Part of #329

--- server/frontend/downloads-main.js
+++ server/frontend/downloads-main.js
@@ -1,101 +1,118 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import {createRoot} from "react-dom/client";
 import Fuse from "fuse.js";
 
 const root = createRoot(document.getElementById("root"));
 
 const ACTIVE_VISIBLE_DEVICES = JSON.parse(
-  document.getElementById("active-visible-devices").textContent,
+    document.getElementById("active-visible-devices").textContent,
 );
 
 function App() {
-  let [searchTerm, setSearchTerm] = React.useState("");
-  return (
-    <>
-      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <DownloadList filter={searchTerm} />
-    </>
-  );
+    let [searchTerm, setSearchTerm] = React.useState("");
+    let [showUnmaintained, setShowUnmaintained] = React.useState(false);
+
+    return (
+        <>
+            <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+            <OptionsBar showUnmaintained={showUnmaintained} setShowUnmaintained={setShowUnmaintained}/>
+            <DownloadList filter={searchTerm} showUnmaintained={showUnmaintained}/>
+        </>
+    );
 }
 
-function SearchBar({ searchTerm, setSearchTerm }) {
-  function changedInput(event) {
-    setSearchTerm(event.target.value);
-  }
+function SearchBar({searchTerm, setSearchTerm}) {
+    function changedInput(event) {
+        setSearchTerm(event.target.value);
+    }
 
-  return (
-    <input
-      className="form-control"
-      type="text"
-      placeholder="Search for devices"
-      value={searchTerm}
-      onChange={changedInput}
-    />
-  );
+    return (
+        <input
+            className="form-control"
+            type="text"
+            placeholder="Search for devices"
+            value={searchTerm}
+            onChange={changedInput}
+        />
+    );
 }
 
-function DownloadList({ filter }) {
-  let filtered_devices;
+function OptionsBar({showUnmaintained, setShowUnmaintained}) {
+    return (
+        <label>
+            <input
+                type="checkbox"
+                checked={showUnmaintained}
+                onChange={e => setShowUnmaintained(e.target.checked)}
+            /> Show unmaintained devices
+        </label>
+    )
+}
 
-  if (filter) {
-    // Fuzzy search through all devices
-    filtered_devices = new Fuse(ACTIVE_VISIBLE_DEVICES, {keys: ['name']}).search(filter);
+function DownloadList({filter, showUnmaintained}) {
+    let filtered_devices = ACTIVE_VISIBLE_DEVICES;
 
-    // Map results returned from Fuse.js to remove item enclosing
-    filtered_devices = filtered_devices.map((result) => result.item)
-  } else {
-    filtered_devices = ACTIVE_VISIBLE_DEVICES;
-  }
+    if (!showUnmaintained) {
+        filtered_devices = filtered_devices.filter(device => device["enabled"]);
+    }
 
-  function sortActive(a, b) {
-    return (a["enabled"] === b["enabled"]) ? 0 : a["enabled"] ? -1 : 1;
-  }
+    if (filter) {
+        // Fuzzy search through all devices
+        filtered_devices = new Fuse(filtered_devices, {keys: ['name']}).search(filter);
 
-  function sortName(a, b) {
-    return a["name"] > b["name"];
-  }
+        // Map results returned from Fuse.js to remove item enclosing
+        filtered_devices = filtered_devices.map((result) => result.item)
+    }
 
-  filtered_devices = filtered_devices.sort(sortName).sort(sortActive);
+    function sortActive(a, b) {
+        return (a["enabled"] === b["enabled"]) ? 0 : a["enabled"] ? -1 : 1;
+    }
 
-  return (
-    <div
-      className="container"
-      style={{ marginTop: "5px", marginBottom: "30px" }}
-    >
-      <div className="row row-cols-1 row-cols-md-4 g-4">
-        {filtered_devices.map((device) => {
-          let device_card_style = device.enabled
-            ? "col"
-            : "col disabled-device-card";
-          let device_photo_url =
-            device.photo_url === ""
-              ? "static/img/no_device_image.png"
-              : device.photo_url;
-          let device_card_title_style = device.enabled
-            ? "card-title"
-            : "card-title text-reset text-decoration-line-through";
-          return (
-            <div className={device_card_style} key={device.codename}>
-              <a className="card-url" href={device.url}>
-                <div className="card h-100">
-                  <img src={device_photo_url} className="card-img-top"></img>
-                  <div className="card-body">
-                    <h5 className={device_card_title_style}>{device.name}</h5>
-                  </div>
-                </div>
-              </a>
+    function sortName(a, b) {
+        return a["name"] > b["name"];
+    }
+
+    filtered_devices = filtered_devices.sort(sortName).sort(sortActive);
+
+    return (
+        <div
+            className="container"
+            style={{marginTop: "5px", marginBottom: "30px"}}
+        >
+            <div className="row row-cols-1 row-cols-md-4 g-4">
+                {filtered_devices.map((device) => {
+                    let device_card_style = device.enabled
+                        ? "col"
+                        : "col disabled-device-card";
+                    let device_photo_url =
+                        device.photo_url === ""
+                            ? "static/img/no_device_image.png"
+                            : device.photo_url;
+                    let device_card_title_style = device.enabled
+                        ? "card-title"
+                        : "card-title text-reset text-decoration-line-through";
+                    return (
+                        <div className={device_card_style} key={device.codename}>
+                            <a className="card-url" href={device.url}>
+                                <div className="card h-100">
+                                    <img src={device_photo_url} className="card-img-top"></img>
+                                    <div className="card-body">
+                                        <h5 className={device_card_title_style}>{device.name}</h5>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    );
+                })}
             </div>
-          );
-        })}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>,
 );
 
 const build_date = `Built on ${BUILD_DATE}`;

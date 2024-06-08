30338a413ab0abf5b5b5077e95f0e42152477949
Eric Park <me@ericswpark.com>
2024-06-08 15:09:57 +0900

    fix(server): remove strikethrough text on disabled devices (#329)
    Disabled devices are hidden by default, so we don't need to
    strikethrough them.
    
    Closes #329

--- server/frontend/downloads-main.js
+++ server/frontend/downloads-main.js
@@ -88,16 +88,13 @@ function DownloadList({filter, showUnmaintained}) {
                         device.photo_url === ""
                             ? "static/img/no_device_image.png"
                             : device.photo_url;
-                    let device_card_title_style = device.enabled
-                        ? "card-title"
-                        : "card-title text-reset text-decoration-line-through";
                     return (
                         <div className={device_card_style} key={device.codename}>
                             <a className="card-url" href={device.url}>
                                 <div className="card h-100">
                                     <img src={device_photo_url} className="card-img-top"></img>
                                     <div className="card-body">
-                                        <h5 className={device_card_title_style}>{device.name}</h5>
+                                        <h5 className="card-title">{device.name}</h5>
                                     </div>
                                 </div>
                             </a>

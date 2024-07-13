dbdf83267b1865f301aa49883232aac435c34b2b
Eric Park <me@ericswpark.com>
2024-07-13 19:37:39 +0900

    fix(server): remove OpenAPI schema generation
    The functionality was deprecated in django-rest-framework, so we're
    proactively removing it. Testing shows that it never worked right
    without the proper dependencies, which we never shipped with shipper, so
    removing it is not that big of a deal. We can evaluate adding other
    options if the need arises.

--- server/api/urls/__init__.py
+++ server/api/urls/__init__.py
@@ -1,17 +1,6 @@
 from django.urls import path, include
-from rest_framework.schemas import get_schema_view
-
-from config import settings
 
 urlpatterns = [
-    # OpenAPI schema
-    path(
-        "openapi/",
-        get_schema_view(
-            title="shipper", description="shipper API", version=settings.SHIPPER_VERSION
-        ),
-        name="openapi-schema",
-    ),
     path("latest/", include("api.urls.latest")),
     path("v1/", include("api.urls.v1")),
     path("v2/", include("api.urls.v2")),

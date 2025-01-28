7f5e99e69eeb691972e1fc8603eb00487ed17592
Eric Park <me@ericswpark.com>
2025-01-28 16:48:22 -0500

    fix(server): close img tag properly and add alt text

--- server/frontend/components/DeviceCard.tsx
+++ server/frontend/components/DeviceCard.tsx
@@ -13,7 +13,7 @@ export default function DeviceCard({ device }: { device: Device }) {
     <div className={device_card_style} key={device.codename}>
       <a className="card-url" href={device.url}>
         <div className="card h-100">
-          <img src={device_photo_url} className="card-img-top"></img>
+          <img src={device_photo_url} className="card-img-top" alt={`${device.name}`}/>
           <div className="card-body">
             <h5 className="card-title">{device.name}</h5>
           </div>

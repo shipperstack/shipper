6922beb213d1d76b87e8996d5638f7f2455a9aa3
Eric Park <me@ericswpark.com>
2025-04-25 16:08:11 -0400

    test(server): add admin mirror status view tests

--- server/internaladmin/tests.py
+++ server/internaladmin/tests.py
@@ -3,7 +3,11 @@ from django.test import RequestFactory, TestCase
 from core.tests.base import mock_setup
 from django.contrib.auth import get_user_model
 
-from internaladmin.views import AdminStatisticsView, get_humanized_total_size
+from internaladmin.views import (
+    AdminBuildMirrorStatusView,
+    AdminStatisticsView,
+    get_humanized_total_size,
+)
 
 User = get_user_model()
 
@@ -42,5 +46,19 @@ class InternalAdminViewTestCase(TestCase):
         request = self.factory.get("")
         request.user = AnonymousUser()
         response = AdminStatisticsView.as_view()(request)
+
+        self.assertEqual(response.status_code, 302)
+
+    def test_admin_mirror_status_view(self):
+        request = self.factory.get("")
+        request.user = self.user
+        response = AdminBuildMirrorStatusView.as_view()(request)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_admin_mirror_status_view_no_permissions(self):
+        request = self.factory.get("")
+        request.user = AnonymousUser()
+        response = AdminBuildMirrorStatusView.as_view()(request)
 
         self.assertEqual(response.status_code, 302)

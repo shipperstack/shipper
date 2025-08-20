b2ab221200c1f97052cf262f10e6d121fc63efcc
Eric Park <me@ericswpark.com>
2025-08-20 19:22:17 -0400

    feat(server): add thumbhash on client-side
    Closes #220

--- server/frontend/components/DeviceCard.tsx
+++ server/frontend/components/DeviceCard.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { UnLazyImage } from "@unlazy/react";
 
 import type { Device } from "./DownloadList";
 
@@ -13,7 +14,13 @@ export default function DeviceCard({ device }: { device: Device }) {
     <div className={device_card_style} key={device.codename}>
       <a className="card-url" href={device.url}>
         <div className="card h-100">
-          <img src={device_photo_url} className="card-img-top" alt={`${device.name}`}/>
+          <UnLazyImage
+            thumbhash={device.photo_thumbhash}
+            src={device_photo_url}
+            className="card-img-top"
+            alt={`${device.name}`}
+            autoSizes
+          />
           <div className="card-body">
             <h5 className="card-title">{device.name}</h5>
           </div>
